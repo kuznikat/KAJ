7a987bbd4a7d99f7e530c301022ed9fc0ffd2813
kuznikat <kuznikat@fel.cvut.cz>
2025-05-21 17:30:22 +0200

    add transaction conduction

---
+++ home.js
@@ -0,0 +1,39 @@
+const balance = document.getElementById('balance');
+
+const money_income = document.getElementById('money-income');
+const money_outcome = document.getElementById('money-outcome');
+
+const list = document.getElementById('list');
+const form = document.getElementById('list');
+const rext = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+
+const scamTransactions = [
+    { id: 1, text: 'Grocery', amount: -560 },
+    { id: 2, text: 'Salary', amount: -25000 },
+    { id: 3, text: 'Cinema', amount: -210 },
+    { id: 4, text: 'Gym', amount: -350 }
+];
+
+let Transactions = scamTransactions;
+
+function addTransactionDom(transaction) {
+    const sign = transaction[0].amount < 0 ? "-" : "+";
+    const item = document.createElement("li");
+
+    item.classList.add(
+        transaction[0].amount > 0 ? "outcome" : "income"
+    )
+
+
+    item.innerHTML = `
+    ${transaction[0].text}<span>${sign}${Math.abs(transaction[0].amount)}</span>
+    <button class="delete-btn" onclik="">X</button>
+    `;
+
+    list.appendChild(item);
+}
+
+addTransactionDom(Transactions);
+
